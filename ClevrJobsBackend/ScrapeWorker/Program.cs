63b1eb16ba07ff4600d212f977723f04027c9d13
Jonathan Bäckhed <1368618+jonathanbackhed@users.noreply.github.com>
2026-01-12 13:00:21 +0100

    feat: add database support to ScrapeWorker (#1)

--- ClevrJobsBackend/ScrapeWorker/Program.cs
+++ ClevrJobsBackend/ScrapeWorker/Program.cs
@@ -1,7 +1,12 @@
+using Data;
+using Microsoft.EntityFrameworkCore;
 using ScrapeWorker;
 
 var builder = Host.CreateApplicationBuilder(args);
 builder.Services.AddHostedService<Worker>();
+
+builder.Services.AddData(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var host = builder.Build();
 host.Run();
