fa6ba7c061010612042f8ce0e2594078ac881deb
Jonathan Bäckhed <1368618+jonathanbackhed@users.noreply.github.com>
2026-01-13 01:14:27 +0100

    feat: check last scrape before run (#18)

--- ClevrJobsBackend/ScrapeWorker/Worker.cs
+++ ClevrJobsBackend/ScrapeWorker/Worker.cs
@@ -1,16 +1,38 @@
+using Data.Repositories;
+
 namespace ScrapeWorker
 {
-    public class Worker(ILogger<Worker> logger) : BackgroundService
+    public class Worker : BackgroundService
     {
+        private readonly ILogger<Worker> _logger;
+        private readonly IServiceScopeFactory _scopeFactory;
+
+        public Worker(ILogger<Worker> logger, IServiceScopeFactory scopeFactory)
+        {
+            _logger = logger;
+            _scopeFactory = scopeFactory;
+        }
+
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             while (!stoppingToken.IsCancellationRequested)
             {
-                if (logger.IsEnabled(LogLevel.Information))
+                using var scope = _scopeFactory.CreateScope();
+                var jobRepository = scope.ServiceProvider.GetRequiredService<IJobRepository>();
+
+                var lastRun = await jobRepository.GetLastScrapeRun();
+
+                if (lastRun != null && lastRun.StartedAt.Date == DateTime.Now.Date)
                 {
-                    logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                    _logger.LogInformation("Scrape already performed today. Waiting until tomorrow.");
+
+                    var now = DateTime.Now;
+                    var targetTime = now.Date.AddDays(1).AddHours(2);
+                    var delayUntilTarget = targetTime - now;
+
+                    await Task.Delay(delayUntilTarget, stoppingToken);
+                    continue;
                 }
-                await Task.Delay(1000, stoppingToken);
             }
         }
     }
