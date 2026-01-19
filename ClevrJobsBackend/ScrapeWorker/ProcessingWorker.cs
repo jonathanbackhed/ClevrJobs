5f4cae3afcfb71929c0ec55f5b9c9cfcf0387311
Jonathan Bäckhed <1368618+jonathanbackhed@users.noreply.github.com>
2026-01-19 12:20:01 +0100

    feat: add event listener (#29)

--- ClevrJobsBackend/ScrapeWorker/ProcessingWorker.cs
+++ ClevrJobsBackend/ScrapeWorker/ProcessingWorker.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Data.Repositories;
+using Queue.Messages;
+using Queue.Services;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,10 +9,39 @@ namespace Workers
 {
     public class ProcessingWorker : BackgroundService
     {
+        private readonly ILogger<ProcessingWorker> _logger;
+        private readonly IServiceScopeFactory _scopeFactory;
+        private readonly IMessageQueue _messageQueue;
+
+        public ProcessingWorker(ILogger<ProcessingWorker> logger, IServiceScopeFactory scopeFactory, IMessageQueue messageQueue)
+        {
+            _logger = logger;
+            _scopeFactory = scopeFactory;
+            _messageQueue = messageQueue;
+        }
+
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            //throw new NotImplementedException();
-            await Task.Delay(10000, stoppingToken);
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                _logger.LogInformation("Waiting for ScrapeCompletedEvent event...");
+                await _messageQueue.SubscribeAsync<ScrapeCompletedEvent>(async evt =>
+                {
+                    _logger.LogInformation("ScrapeCompletedEvent event recieved");
+                    try
+                    {
+                        using var scope = _scopeFactory.CreateScope();
+                        var jobRepository = scope.ServiceProvider.GetRequiredService<IJobRepository>();
+
+                    }
+                    catch (Exception e)
+                    {
+                        _logger.LogError(e, "Error occurred during processing");
+                        await Task.Delay(TimeSpan.FromMinutes(1), stoppingToken);
+                    }
+                },
+                stoppingToken);
+            }
         }
     }
 }
