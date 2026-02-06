33f9be63a7d12bdcc097ee0fe47e1078a0c1cc38
Jonathan Bäckhed <1368618+jonathanbackhed@users.noreply.github.com>
2026-02-06 15:09:23 +0100

    impr: add modal animation (#75)

--- client/src/components/ui/Modal.tsx
+++ client/src/components/ui/Modal.tsx
@@ -27,11 +27,11 @@ export default function Modal({ children, isOpen, close }: Props) {
   return (
     <div
       className={cn(
-        "fixed top-0 right-0 bottom-0 left-0 z-50 h-screen w-screen items-center justify-center bg-black/30 backdrop-blur-sm transition-all duration-1000",
+        "fixed top-0 right-0 bottom-0 left-0 z-50 h-screen w-screen items-center justify-center bg-black/30 backdrop-blur-sm",
         isOpen ? "flex" : "hidden",
       )}
     >
-      <div className="bg-cream relative max-w-3xl rounded-2xl p-10">
+      <div className="bg-cream animate-slide-in-from-top relative max-w-3xl rounded-2xl p-10">
         <div className="absolute top-3 right-3">
           <CustomButton type="button" action={() => close(false)} variant="none" customStyles="p-0 hover:opacity-60">
             <X size={22} />
