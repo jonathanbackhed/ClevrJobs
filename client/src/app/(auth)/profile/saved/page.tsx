d9c8568ceaf5c744001f7e38cb062cad012398ce
Jonathan Bäckhed <1368618+jonathanbackhed@users.noreply.github.com>
2026-02-22 13:33:48 +0100

    fix: add missing toast (#94)

--- client/src/app/(auth)/profile/saved/page.tsx
+++ client/src/app/(auth)/profile/saved/page.tsx
@@ -4,6 +4,7 @@ import JobListItem from "@/components/layout/JobListItem";
 import Pagination from "@/components/layout/Pagination";
 import BackButton from "@/components/ui/BackButton";
 import PulsatingText from "@/components/ui/PulsatingText";
+import Toast from "@/components/ui/Toast";
 import { useSavedJobs } from "@/hooks/useSaved";
 import { SavedJobResponse } from "@/types/saved";
 import { notFound, useRouter, useSearchParams } from "next/navigation";
@@ -23,26 +24,29 @@ export default function Saved() {
   if (!data) notFound();
 
   return (
-    <div className="mx-auto flex min-h-screen max-w-3xl flex-col px-4 py-12 pb-20 sm:px-6 sm:py-16">
-      <div className="flex flex-1 flex-col gap-4">
-        {/* <BackButton text="Gå tillbaka" backFunction={() => router.push("/profile")} /> */}
-        {data?.items && data.items.length < 1 && (
-          <span className="text-center text-xl font-bold">Inga sparade jobb hittades</span>
-        )}
-        {data.items.map((savedJob: SavedJobResponse, index: number) => (
-          <JobListItem key={savedJob.id} job={savedJob.jobListingMini} index={index} />
-        ))}
-      </div>
+    <>
+      <Toast />
+      <div className="mx-auto flex min-h-screen max-w-3xl flex-col px-4 py-12 pb-20 sm:px-6 sm:py-16">
+        <div className="flex flex-1 flex-col gap-4">
+          {/* <BackButton text="Gå tillbaka" backFunction={() => router.push("/profile")} /> */}
+          {data?.items && data.items.length < 1 && (
+            <span className="text-center text-xl font-bold">Inga sparade jobb hittades</span>
+          )}
+          {data.items.map((savedJob: SavedJobResponse, index: number) => (
+            <JobListItem key={savedJob.id} job={savedJob.jobListingMini} index={index} />
+          ))}
+        </div>
 
-      {data.items.length > data.pageSize && (
-        <Pagination
-          totalPages={data?.totalPages ?? 0}
-          totalCount={data?.totalCount ?? 0}
-          pageSize={data?.pageSize ?? 0}
-          currentPage={currentPage}
-          onPageChange={setCurrentPage}
-        />
-      )}
-    </div>
+        {data.items.length > data.pageSize && (
+          <Pagination
+            totalPages={data?.totalPages ?? 0}
+            totalCount={data?.totalCount ?? 0}
+            pageSize={data?.pageSize ?? 0}
+            currentPage={currentPage}
+            onPageChange={setCurrentPage}
+          />
+        )}
+      </div>
+    </>
   );
 }
