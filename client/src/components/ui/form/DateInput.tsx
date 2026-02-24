5dc0998c677019b3ed3d5176756f3a4a58b06335
Jonathan Bäckhed <1368618+jonathanbackhed@users.noreply.github.com>
2026-02-24 16:11:57 +0100

    feat: add more types to dateinput

--- client/src/components/ui/form/DateInput.tsx
+++ client/src/components/ui/form/DateInput.tsx
@@ -5,12 +5,13 @@ import { FieldError } from "react-hook-form";
 interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
   customStyles?: string;
   errors?: FieldError;
+  type?: "datetime-local" | "month" | "time" | "week";
 }
 
-export default function DateInput({ customStyles, errors, ...rest }: Props) {
+export default function DateInput({ customStyles, errors, type, ...rest }: Props) {
   return (
     <input
-      type="date"
+      type={type ? type : "date"}
       className={cn(
         "bg-cream-light rounded-lg border border-stone-600 p-2.5 ring-stone-800 focus:ring-offset-0 focus-visible:ring-1",
         errors && "border-red-600 ring-red-600 dark:border-red-800 dark:ring-red-800",
